fe80b9b95c6a6477ef468491df8125587335356f
nevillealee <nevillealee@gmail.com>
2017-07-16 00:36:43 +0000

    sendgrid added with heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,13 @@
 Rails.application.configure do
+  ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
   
   config.action_mailer.default_url_options = { host: 'realfood-neville-lee.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
