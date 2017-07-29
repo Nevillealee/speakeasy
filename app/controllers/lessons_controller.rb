373e6823124d58ea04faa3d054825f1f65886b24
nevillealee <nevillealee@gmail.com>
2017-07-29 23:40:42 +0000

    issue with autenticate user and redirect to in lessons controller/courses/show page

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,23 @@
 class LessonsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :enrolled
     
     def show
     end
     
-    helper_method :current_lesson
+    helper_method :current_lesson, :enrolled
       def current_lesson
         @current_lesson ||= Lesson.find(params[:id])
       end
+      
+      def enrolled
+        @course = current_lesson.section.course
+        if current_user.enrolled_in?(@course)
+          return current_user.enrolled_in?(@course)
+        else 
+          redirect_to course_path, alert: 'You need to enroll in  order to access this lesson'
+        end
+      end
+      
+      
 end
