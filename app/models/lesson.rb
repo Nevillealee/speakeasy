b080f70d5a23973b445d02e2d76683fdc4f14a42
nevillealee <nevillealee@gmail.com>
2017-08-02 04:52:27 +0000

    added ranked-model gem to rank lessons by row_order with matching section_id

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,7 @@
 class Lesson < ApplicationRecord
+    include RankedModel
     belongs_to :section
     mount_uploader :video, VideoUploader
+    ranks :row_order, with_same: :section_id
+        
 end
