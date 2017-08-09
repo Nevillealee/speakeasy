921b8a1a2a018c41266a4a181a798124aeeb8279
nevillealee <nevillealee@gmail.com>
2017-08-09 22:32:05 +0000

    support for sections ranked gem

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,8 @@
 class Instructor::SectionsController < ApplicationController
     before_action :authenticate_user!
     before_action :require_authorized_for_current_course
+    before_action :require_authorized_for_current_section, only: [:update]
+    
     
     def new
         @section = Section.new
@@ -9,6 +11,10 @@ class Instructor::SectionsController < ApplicationController
     def create
         @section = current_course.sections.create(section_params)
         redirect_to instructor_course_path(current_course)
+    end
+    def update
+        current_course.update_attributes(section_params)
+        render plain: "updated!"
     end
     
     private
@@ -19,13 +25,22 @@ class Instructor::SectionsController < ApplicationController
         end
     end
     
-    helper_method :current_course
+    def require_authorized_for_current_section 
+        if current_section.course.user != current_user
+          render plain: "Unauthorized", status: :unauthorized
+        end
+    end
     
+    helper_method :current_course
     def current_course
-        @current_course || Course.find(params[:course_id])
+        if params[:course_id]
+           @current_course ||= Course.find(params[:course_id])
+         else
+           current_section.course
+        end
     end
     
     def section_params
-        params.require(:section).permit(:title)
+        params.require(:section).permit(:title, :row_order_position)
     end
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,4 +1,6 @@
 class Section < ApplicationRecord
+    include RankedModel
     belongs_to :course
     has_many :lessons
+    ranks :row_order, with_same: :course_id
 end
