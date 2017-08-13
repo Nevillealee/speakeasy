71fd8ce7f00108c0ca67a06e9812ce8c08bb4dbc
nevillealee <nevillealee@gmail.com>
2017-08-13 05:50:39 +0000

    instructor_sections controller edited to remove new function and subsequent view

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -16,7 +16,7 @@ class Instructor::CoursesController < ApplicationController
     end
     
     def show
-      
+      @section = Section.new
     end
     
     private
--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,11 @@ Rails.application.routes.draw do
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :lessons, only: [:update]
-    resources :sections, only: [:update]
-    resources :sections, only: [] do
+    resources :sections, only: [:update] do
       resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:new, :create] 
+      resources :sections, only: [:create]
     end
   end
 end
